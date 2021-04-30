943615d8a5f31c2f56556b8d8b122a3d4bef38da
Hugo Slabbert <hugo@slabnet.com>
2021-04-30 04:50:13 +0000

    arrays/slices: SumAllTails()

--- arrays/sum.go
+++ arrays/sum.go
@@ -6,14 +6,33 @@ func Sum(numbers []int) int {
 	for _, number := range numbers {
 		sum += number
 	}
+
 	return sum
 }
 
 // SumAll takes one or more int slices and returns an int slice
 // of the sums of those slices' elements
-func SumAll(numbersToSum ...[]int) (sums []int) {
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
 	}
-	return
+
+	return sums
+}
+
+// SumAllTails takes one or more int slices and returns an int slice
+// of the sums of those slices' elements, minus the first ("head") element.
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
