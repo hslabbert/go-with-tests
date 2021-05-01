522a9027052b8c770f635ea96f9ea32e44dd454c
Hugo Slabbert <hugo@slabnet.com>
2021-05-01 04:35:18 +0000

    structs: actual structs for Perimeter() and Area()

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,13 +1,18 @@
 package shapes
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter takes width and height of a rectangle and
 // returns its perimeter.
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area takes width and height of a rectangle and
 // returns its area.
-func Area(width, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
