20e36be52398801c16083b8fdc466e660ab56086
Hugo Slabbert <hugo@slabnet.com>
2021-05-04 05:04:42 +0000

    maps: initial dictionary use of maps

---
+++ maps/dictionary.go
@@ -0,0 +1,21 @@
+package dictionary
+
+import "errors"
+
+// ErrNotFound is returned if a definition is not found in a
+// Dictionary for a word or phrase.
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+// A Dictionary is a map
+type Dictionary map[string]string
+
+// Search takes a word to look up in the dictionary
+// and returns that word's definition.
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
