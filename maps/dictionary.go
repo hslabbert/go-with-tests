d80faad7fc8e43f82dee4afaf13e0c42301e4a52
Hugo Slabbert <hugo@slabnet.com>
2021-05-05 02:53:17 +0000

    maps: Update() and Delete() methods

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,8 +2,9 @@ package dictionary
 
 // A set of DictionaryErr definitions
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 // A DictionaryErr encapsulates various errors on accessing or
@@ -42,4 +43,25 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 
 	return nil
+}
+
+// Update will update an existing word's definition in a Dictionary.
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// Delete will remove a word from a Dictionary.
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
