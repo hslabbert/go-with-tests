c537f11ddf71e61d1d60cc8d3e869ff5a37e07f7
Hugo Slabbert <hugo@slabnet.com>
2021-05-06 02:44:15 +0000

    mocking: injected Sleeper interface

--- mocking/main.go
+++ mocking/main.go
@@ -10,15 +10,30 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
+// A Sleeper implements a Sleep() method
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper implements a time.Second-driven Sleep method.
+type DefaultSleeper struct{}
+
+// Sleep implements a time.Second-driven method on DefaultSleeper.
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 // Countdown takes an io.Writer write a countdown from 3, 2, 1, Go!
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
