ad1d17ec1f59468f54a21d88dcc54146fe297b8b
Hugo Slabbert <hugo@slabnet.com>
2021-05-06 02:31:05 +0000

    mocking: add sleep to Countdown()

--- mocking/main.go
+++ mocking/main.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
+// Countdown takes an io.Writer write a countdown from 3, 2, 1, Go!
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
