8a4a33299efacc272c9debc35edf768102865d58
Hugo Slabbert <hugo@slabnet.com>
2021-05-07 02:36:08 +0000

    concurrency: doc it

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,5 +1,7 @@
 package concurrency
 
+// A WebsiteChecker takes a URL as a string and returns a bool
+// for whether it passed its check.
 type WebsiteChecker func(string) bool
 
 type result struct {
@@ -7,6 +9,9 @@ type result struct {
 	bool
 }
 
+// CheckWebsites takes a WebsiteChecker and a slice of URLs to check,
+// runs them through the WebsiteChecker, and returns a map of
+// the URLs and whether they passed checks (as bools).
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	resultChannel := make(chan result)
