8c54df2deb48d09af97140e2d300119c9cdb8ee5
Hugo Slabbert <hugo@slabnet.com>
2021-05-07 05:53:39 +0000

    reflection: add channels

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -24,7 +24,12 @@ func walk(x interface{}, fn func(input string)) {
 		for _, key := range val.MapKeys() {
 			walkValue(val.MapIndex(key))
 		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
+		}
 	}
+
 }
 
 func getValue(x interface{}) reflect.Value {
