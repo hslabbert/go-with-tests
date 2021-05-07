69d68e4920a013b478b98f146a2bb0b3b874f1d3
Hugo Slabbert <hugo@slabnet.com>
2021-05-07 03:02:25 +0000

    select: introduce net/http/httptest

--- select/racer.go
+++ select/racer.go
@@ -10,17 +10,18 @@ import (
 // two URLs by sending an HTTP GET request to both of them
 // and seeing which responds more quickly.
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
