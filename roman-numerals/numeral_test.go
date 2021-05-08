3438df241f6c468f62d06bb013ad5cab0f177006
Hugo Slabbert <hugo@slabnet.com>
2021-05-08 23:37:37 +0000

    property-based: initial switch-based roman numerals

---
+++ roman-numerals/numeral_test.go
@@ -0,0 +1,35 @@
+package numerals
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{Description: "1 gets converted to I", Arabic: 1, Want: "I"},
+		{Description: "2 gets converted to II", Arabic: 2, Want: "II"},
+		{Description: "3 gets converted to III", Arabic: 3, Want: "III"},
+		{Description: "4 gets converted to IV (can't repeat more than 3 times", Arabic: 4, Want: "IV"},
+		{Description: "5 gets converted to V", Arabic: 5, Want: "V"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			want := test.Want
+
+			assertNumerals(t, got, want)
+		})
+	}
+}
+
+func assertNumerals(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
---
+++ roman-numerals/roman_numerals.go
@@ -0,0 +1,26 @@
+package numerals
+
+import "strings"
+
+// ConvertToRoman takes and integer and returns its
+// Roman Numeral equivalent.
+func ConvertToRoman(arabic int) string {
+
+	var result strings.Builder
+
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
+	}
+
+	return result.String()
+}
