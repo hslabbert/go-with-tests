48360d1943587131136bdf62b9527c71f9b2f791
Hugo Slabbert <hugo@slabnet.com>
2021-05-08 16:41:14 +0000

    sync: add mutex to Counter struct

--- sync/sync.go
+++ sync/sync.go
@@ -1,12 +1,17 @@
 package sync
 
+import "sync"
+
 // A Counter can be incremented and holds a counter value.
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 // Inc takes no aruguments, and will increment a Counter's value.
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing counter 3 times leaves it at 3", func(t *testing.T) {
@@ -9,11 +12,29 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		assertCount(t, counter, 3)
+		assertCount(t, &counter, 3)
+	})
+
+	t.Run("it runs safe concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCount(t, &counter, wantedCount)
 	})
 }
 
-func assertCount(t testing.TB, got Counter, want int) {
+func assertCount(t testing.TB, got *Counter, want int) {
 	t.Helper()
 
 	if got.Value() != want {
