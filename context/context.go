847ddac739c9d7db206a4e5bcb1f139eb1970672
Hugo Slabbert <hugo@slabnet.com>
2021-05-08 21:06:26 +0000

    context: initial context with cancellation in Server

--- context/context.go
+++ context/context.go
@@ -8,11 +8,24 @@ import (
 // A Store can Fetch things.
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
 // Server takes a Store and returns an http.HandlerFunc
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
