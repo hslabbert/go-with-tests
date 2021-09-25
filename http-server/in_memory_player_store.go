7d6d8bf6a88e49c4915fe5c6b61349b7efc979c5
Hugo Slabbert <hugo@slabnet.com>
2021-09-24 22:29:27 -0700

    chore: fix up docs on http-server in_memory_player_store.go

--- http-server/in_memory_player_store.go
+++ http-server/in_memory_player_store.go
@@ -2,17 +2,24 @@ package main
 
 import "sync"
 
+// NewInMemoryPlayerStore initializes and returns an
+// *InMemoryPlayerStore in order to set up the needed
+// map and mutex.
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{
 		store: map[string]int{},
 		mu:    sync.RWMutex{}}
 }
 
+// An InMemoryPlayerStore is a PlayerStore that provides only
+// ephemeral storage of player scores.
 type InMemoryPlayerStore struct {
 	store map[string]int
 	mu    sync.RWMutex
 }
 
+// RecordWin adds one win to the provided player's score in the
+// provided *InMemoryPlayerStore.
 func (i *InMemoryPlayerStore) RecordWin(name string) error {
 	i.mu.Lock()
 	defer i.mu.Unlock()
@@ -20,6 +27,8 @@ func (i *InMemoryPlayerStore) RecordWin(name string) error {
 	return nil
 }
 
+// GetPlayerScore returns a player's score from the provided
+// *InMemoryPlayerStore.
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	i.mu.RLock()
 	defer i.mu.RUnlock()
