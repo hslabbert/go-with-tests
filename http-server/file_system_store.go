87b25ee67b15f7c694258c2ff95c6a06a9958ec5
Hugo Slabbert <hugo@slabnet.com>
2021-09-25 21:40:00 -0700

    fix: make FileSystemPlayerStore use a ReadSeeker
    * Read() on a Reader advances the offset of the stream as it reads
    * That means it's a onetime operation
    * ReadSeeker can use a Seek on the Seeker to reset the offset to
    "rewind" to the beginning of the Reder stream.

--- http-server/file_system_store.go
+++ http-server/file_system_store.go
@@ -8,13 +8,13 @@ import (
 // interface with filesystem backing, storing the
 // Player data in a json file on disk.
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 // GetLeague returns a []Player of players stored in
 // the provided *FileSystemPlayerStore
 func (f *FileSystemPlayerStore) GetLeague() []Player {
-	var league []Player
-	league, _ = NewLeague(f.database)
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
 	return league
 }
