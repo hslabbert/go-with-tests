1fec4ca171e9a9602124a971640c751381aaf45a
Hugo Slabbert <hugo@slabnet.com>
2021-09-26 11:38:58 -0700

    fix: support new players in FileSystemPlayerStore

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -45,6 +45,14 @@ func TestFileSystemStore(t *testing.T) {
 		want := 34
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("store wins for new players", func(t *testing.T) {
+		store.RecordWin("Pepper")
+
+		got := store.GetPlayerScore("Pepper")
+		want := 1
+		assertScoreEquals(t, got, want)
+	})
 }
 
 func assertScoreEquals(t testing.TB, got, want int) {
