3c0ac2da4d5bc42f4fea669720c7592ca7a99299
Hugo Slabbert <hugo@slabnet.com>
2021-09-26 21:15:54 -0700

    chore: FileSystemPlayerStoreFromFile() to abstract db file handling

--- http-server/file_system_store.go
+++ http-server/file_system_store.go
@@ -99,3 +99,26 @@ func initializePlayerDBFile(file *os.File) error {
 
 	return nil
 }
+
+// FileSystemPlayerStoreFromFile constructs a FileSystemPlayerStore from
+// the provided file path, with the needed func() to close the
+// FileSystemPlayerStore json db file.
+func FileSystemPlayerStoreFromFile(path string) (*FileSystemPlayerStore, func(), error) {
+	db, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem opening %s %v", path, err)
+	}
+
+	closeFunc := func() {
+		db.Close()
+	}
+
+	store, err := NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem creating file system player store %v", err)
+	}
+
+	return store, closeFunc, err
+}
